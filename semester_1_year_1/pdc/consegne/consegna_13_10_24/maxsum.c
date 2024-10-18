efd23da0c8f1f559ee34b68b9805816e26a53604
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2024-10-18 09:00:30 +0200

    update note pdc (cluster munnezza)

--- semester_1_year_1/pdc/consegne/consegna_13_10_24/maxsum.c
+++ semester_1_year_1/pdc/consegne/consegna_13_10_24/maxsum.c
@@ -1,32 +1,30 @@
 // Author: Antonio Petrillo
 // Email: antonio.petrillo4@studenti.unina.it
 // Matricola: N97000496
-#include <omp.h>
 #include <math.h>
+#include <omp.h>
 
-double maxsum(int N, int LD, double* A, int NT) {
-    double max = 0, local_max = 0, sum = 0;
-    int id, i, j;
-    const int portion = N / NT;
+double maxsum(int N, int LD, double *A, int NT) {
+  double max = 0, local_max = 0, sum = 0;
+  int id, row, col;
+  const int portion = N / NT;
 
-    omp_set_num_threads(NT);
+  omp_set_num_threads(NT);
 
-    #pragma omp parallel private(local_max, id, sum, i, j)
-    {
-        id = omp_get_thread_num();
-        for (i = portion * id; i < portion * (id + 1); i++) {
-            sum = 0;
-            for (j = 0; j < N; j++) {
-                sum += sqrt(A[i * LD + j]);
-            }
-            local_max = local_max < sum ? sum : local_max;
-        }
-
-        #pragma omp critical
-        {
-            max = max < local_max ? local_max : max;
-        }
+#pragma omp parallel private(local_max, id, sum, row, col)
+  {
+    id = omp_get_thread_num();
+    for (row = portion * id; row < portion * (id + 1); row++) {
+      sum = 0;
+      for (col = 0; col < N; col++) {
+        sum += sqrt(A[row * LD + col]);
+      }
+      local_max = local_max < sum ? sum : local_max;
     }
 
-    return max;
+#pragma omp critical
+    { max = max < local_max ? local_max : max; }
+  }
+
+  return max;
 }
